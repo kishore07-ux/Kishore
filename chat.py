552a62be4347e8a511eeab693d228e75a9e0ab71
kishore07-ux <k7418435@gmail.com>
2026-09-04 10:08:10 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,8 +1,8 @@
 import streamlit as st
 a=st.chat_input("Enter your command:")
 if a:
-	st.chat_message("user").write(a)
+	st.chat_message("user",avatar="user.png").write(a)
 	if a.lower()=="hi":
-		st.chat_message("ai").write("hello")
+		st.chat_message("ai",avatar="bot.png").write("hello")
 	elif a.lower()=="bye":
-		st.chat_message("ai").write("goodbye")
+		st.chat_message("ai",avatar="bot.png").write("goodbye")
